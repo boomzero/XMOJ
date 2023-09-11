4b594aa92be3555150008fafbb11f7834d9e24ae
Zhu Chenrui <85378277+boomzero@users.noreply.github.com>
2023-09-11 21:58:08 +0800

    Create 3038.cpp

---
+++ 3038.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double dtr(double in) {
+    return in / 180 * M_PI;
+}
+
+int main() {
+    freopen("physics.in", "r", stdin);
+    freopen("physics.out", "w", stdout);
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        double theta, v, d, g;
+        //cin >> theta >> v >> d >> g;
+        scanf("%lf%lf%lf%lf", &theta, &v, &d, &g);
+        double ans = 2 * (cos(dtr(theta)) * v) * (cos(dtr(90 - theta)) * v) / g;
+        while (v > 0.000001) { //TODO:adjust this!
+            v *= d;
+            ans += 2 * (cos(dtr(theta)) * v) * (cos(dtr(90 - theta)) * v) / g;
+        }
+        printf("%.5lf\n", ans);
+    }
+    return 0;
+}
